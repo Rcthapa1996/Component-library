e14dee890c11687e8b2d1a522d516575b8e6b10f
Rcthapa1996 <Rcthapa1996@gmail.com>
2023-03-17 17:26:44 +0545

    releasing next version using Roll up, Button component is not exposed in npm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Button } from "./stories/Button";
+import { CpButton } from "./stories/CpButton";
+import { Header } from "./stories/Header";
+import { MyButton } from "./stories/MyButton";
+import { Page } from "./stories/Page";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <CpButton /> */}
     <App />
   </React.StrictMode>
 );
@@ -15,3 +22,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export { Button, CpButton, Header, MyButton, Page };
